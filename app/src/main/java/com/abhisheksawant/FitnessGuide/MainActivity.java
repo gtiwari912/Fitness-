73f41fe4967899f3dd9c3f2a1826c0663b5bd48e
Abhishek Sawant <abhishakesawant@gmail.com>
2021-02-10 23:00:49 +0530

    Auto dark/light theme toggle feature added using 'forceDarkAllowed'

--- app/src/main/java/com/abhisheksawant/FitnessGuide/MainActivity.java
+++ app/src/main/java/com/abhisheksawant/FitnessGuide/MainActivity.java
@@ -1,12 +1,16 @@
 package com.abhisheksawant.FitnessGuide;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -17,7 +21,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
 
@@ -33,68 +37,60 @@ public class MainActivity extends AppCompatActivity{
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.bringToFront();
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
-                int id = menuItem.getItemId();
-                if(id == R.id.about_us){
-                    Intent intent = new Intent(MainActivity.this,about_us.class);
-                    startActivity(intent);
-                }
-                else if(id == R.id.about_this_app){
-                    Intent intent = new Intent(MainActivity.this,about_this_app.class);
-                    startActivity(intent);
-                }
-                else if(id == R.id.rate_this_app){
-                    Intent rateintent = new Intent(Intent.ACTION_VIEW);
-                    rateintent.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.abhisheksawant.FitnessGuide"));
-                    startActivity(rateintent);
-                }
-                else if(id == R.id.follow_us){
-                    Intent followintent = new Intent(Intent.ACTION_VIEW);
-                    followintent.setData(Uri.parse("https://www.instagram.com/sawant807/"));
-                    startActivity(followintent);
-                }
-                else if(id == R.id.share_this_app){
-                    Intent shareintent = new Intent(Intent.ACTION_SEND);
-                    shareintent.putExtra(Intent.EXTRA_TEXT,
-                            "https://play.google.com/store/apps/details?id=com.abhisheksawant.FitnessGuide");
-                    shareintent.setType("text/plain");
-                    startActivity(Intent.createChooser(shareintent,"Share Via"));
-                }
-                else if(id == R.id.feedback){
-                    String recipient = "abhishakesawant@gmail.com";
-                    String subject = "Fitness Guide App Feedback";
-                    String body = "Enter your feedback/suggestions/queries here...";
-
-                    Intent feedback_intent = new Intent(Intent.ACTION_VIEW,
-                            Uri.parse("mailto:"+recipient+"?subject="+subject+"&body="+body));
-                    startActivity(Intent.createChooser(feedback_intent,"Send Email Using..."));
-                }
-
-                DrawerLayout drawer = findViewById(R.id.drawer_layout);
-                drawer.closeDrawer(GravityCompat.START);
-
-                return true;
-            }
-        });
-
+        navigationView.setNavigationItemSelectedListener(this);
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home)
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home)
                 .setDrawerLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
 
-
-
-
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.about_us:
+                startActivity(new Intent(MainActivity.this, about_us.class));
+                break;
+            case R.id.about_this_app:
+                startActivity(new Intent(MainActivity.this, about_this_app.class));
+                break;
+            case R.id.rate_this_app:
+                Intent rateIntent = new Intent(Intent.ACTION_VIEW);
+                rateIntent.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.abhisheksawant.FitnessGuide"));
+                startActivity(rateIntent);
+                break;
+            case R.id.follow_us:
+                Intent followIntent = new Intent(Intent.ACTION_VIEW);
+                followIntent.setData(Uri.parse("https://www.instagram.com/sawant807/"));
+                startActivity(followIntent);
+                break;
+            case R.id.share_this_app:
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT,
+                        "https://play.google.com/store/apps/details?id=com.abhisheksawant.FitnessGuide");
+                shareIntent.setType("text/plain");
+                startActivity(Intent.createChooser(shareIntent, "Share Via"));
+                break;
+            case R.id.feedback:
+                String recipient = "abhishakesawant@gmail.com";
+                String subject = "Fitness Guide App Feedback";
+                String body = "Enter your feedback/suggestions/queries here...";
+
+                Intent feedbackIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("mailto:" + recipient + "?subject=" + subject + "&body=" + body));
+                startActivity(Intent.createChooser(feedbackIntent, "Send Email Using..."));
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
 
     @Override
     public boolean onSupportNavigateUp() {
--- app/src/main/java/com/abhisheksawant/FitnessGuide/ui/home/HomeViewModel.java
+++ app/src/main/java/com/abhisheksawant/FitnessGuide/ui/home/HomeViewModel.java
@@ -1,4 +1,4 @@
-package com.abhisheksawant.fitnessmanual.ui.home;
+package com.abhisheksawant.FitnessGuide.ui.home;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
