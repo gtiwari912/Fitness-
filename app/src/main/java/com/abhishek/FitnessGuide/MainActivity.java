30f8e46a167dbc470f6b6a2128ac3b0faba54e57
Abhishek Sawant <abhishakesawant@gmail.com>
2023-04-02 01:57:44 +0530

    renamed app package

---
+++ app/src/main/java/com/abhishek/FitnessGuide/MainActivity.java
@@ -0,0 +1,93 @@
+package com.abhishek.FitnessGuide;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home)
+                .setDrawerLayout(drawer)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.about_us:
+                startActivity(new Intent(MainActivity.this, AboutUs.class));
+                break;
+            case R.id.about_this_app:
+                startActivity(new Intent(MainActivity.this, AboutThisApp.class));
+                break;
+            case R.id.rate_this_app:
+                Intent rateIntent = new Intent(Intent.ACTION_VIEW);
+                rateIntent.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.abhishek.FitnessGuide"));
+                startActivity(rateIntent);
+                break;
+            case R.id.share_this_app:
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT,
+                        "https://play.google.com/store/apps/details?id=com.abhishek.FitnessGuide");
+                shareIntent.setType("text/plain");
+                startActivity(Intent.createChooser(shareIntent, "Share Via"));
+                break;
+            case R.id.feedback:
+                String recipient = "abhishakesawant@gmail.com";
+                String subject = "Fitness Guide App Feedback";
+                String body = "Enter your feedback/suggestions/queries here...";
+
+                Intent feedbackIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("mailto:" + recipient + "?subject=" + subject + "&body=" + body));
+                startActivity(Intent.createChooser(feedbackIntent, "Send Email Using..."));
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+}
